2ae9c5fc8cf08afe8011190d6920ee4d7f6dac9d
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-09-09 16:11:06 +0530

    mentora

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,6 +9,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
+app.get("/api/chat",(req: Request, res:Response)=>{
+  res.send("Hi, how may I help yu today")
+})
+
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on ${PORT}`);
 });
